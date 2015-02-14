60049911b09f61c84882f8b7b15164b818380885
athron <athron.poster@gmail.com>
2015-02-14 11:05:47 +0700

    Prepare Environment

---
+++ src/apm/frmMain.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmMain : Inherits System.Windows.Forms.Form
+
+    Private Sub frmMain_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'frmMain
+        '
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Name = "frmMain"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
